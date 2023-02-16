0147af885cea41494eb97cdf872b9e004943b26f
Tetratronic <aymanebk@gmail.com>
2023-02-16 14:29:30 +0100

    Working addition

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const screen = document.querySelector('.screen');
 
 const buttons = document.querySelector('.buttons');
-
 buttons.addEventListener('click', (event) =>{
     const isButton = event.target.nodeName === 'BUTTON';
+    const isOperation = event.target.className === 'operation';
     if (!isButton){
+        return
+    };
+    if(isOperation && (screen.textContent == '')){
+        alert("NOT ALLOWED, START BY TYPING A NUMBER");
         return;
-    }
+    };
     screen.textContent += event.target.value;
+
+    if (isOperation){
+        operationString = screen.textContent;
+        nums = operationString.split(/[+x/-]/);
+        if(!nums[1]){
+            return;
+        }
+        screen.textContent = add( parseInt(nums[0]), parseInt(nums[1]) )
+    }
 });
 
+function add(a,b){
+    return a + b;
+};
