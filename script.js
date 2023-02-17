d1e283b3ea90d638737df1497db3417728b8a69b
Aymane <aymanebk@gmail.com>
2023-02-17 13:08:25 +0100

    add more operators and improve logic (the code still sucks)

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const screen = document.querySelector('.screen');
-
 const buttons = document.querySelector('.buttons');
+operand =''
+operator = false;
 buttons.addEventListener('click', (event) =>{
     const isButton = event.target.nodeName === 'BUTTON';
     const isOperation = event.target.className === 'operation';
     const clearButton = event.target.id === 'clear'
+    const equalButton = event.target.id === 'equal'
     if (clearButton){
         screen.textContent = ''
     };
@@ -18,17 +20,38 @@ buttons.addEventListener('click', (event) =>{
         return;
     };
 
-    if (isOperation){
-        screen.textContent += event.target.value;
-        operationString = screen.textContent;
-        nums = operationString.split(/[+x/-]/);
-        if(!nums[1] || nums.lenght > 2){
-            return;
-        };
-        screen.textContent = add( parseInt(nums[0]), parseInt(nums[1]) )
+    if (isOperation || equalButton){
+        if(!operator){        
+            screen.textContent += event.target.value;
+            operand = event.target.value;       
+            console.log(operand)
+            operator = true;
+            
+
+        }else{
+            operationString = screen.textContent;
+            nums = operationString.split(/[+x/-]/);
+            console.log("hi");
+            screen.textContent = add( parseInt(nums[0]), parseInt(nums[1]), operand );
+            console.log(add( parseInt(nums[0]), parseInt(nums[1]), operand ));
+            operator = false;
+
+        }
+
+        if(!nums[1] || nums.lenght > 2 ){
+            return;};
+
     };
 });
 
-function add(a,b){
-    return a + b;
+function add(a,b,op){
+    if(op == "+"){
+        return a + b;   
+    }else if(op == "-"){
+        return a - b
+    }else if(op == "x"){
+        return a * b
+    }else if(op == '/'){
+        return a/b
+    }
 };
