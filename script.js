b437e01ec9343af42f8b645d2cd284aa902b49bf
Aymane <aymanebk@gmail.com>
2023-02-17 01:36:27 +0100

    limit displayed characters

--- script.js
+++ script.js
@@ -4,23 +4,29 @@ const buttons = document.querySelector('.buttons');
 buttons.addEventListener('click', (event) =>{
     const isButton = event.target.nodeName === 'BUTTON';
     const isOperation = event.target.className === 'operation';
+    const clearButton = event.target.id === 'clear'
+    if (clearButton){
+        screen.textContent = ''
+    };
     if (!isButton){
         return
     };
-    if(isOperation && (screen.textContent == '')){
-        alert("NOT ALLOWED, START BY TYPING A NUMBER");
+    if(!isOperation){
+        screen.textContent += event.target.value;
+    }
+    if(isOperation && (screen.textContent == '' || screen.textContent.slice(-1).match(/[+x/-]/))){
         return;
     };
-    screen.textContent += event.target.value;
 
     if (isOperation){
+        screen.textContent += event.target.value;
         operationString = screen.textContent;
         nums = operationString.split(/[+x/-]/);
-        if(!nums[1]){
+        if(!nums[1] || nums.lenght > 2){
             return;
-        }
+        };
         screen.textContent = add( parseInt(nums[0]), parseInt(nums[1]) )
-    }
+    };
 });
 
 function add(a,b){
